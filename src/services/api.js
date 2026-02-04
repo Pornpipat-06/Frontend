f9c244ce0ede1b316384ca71e7d3f5e340b7f9bd
Pornpipat-06 <nutchainoi04@gmail.com>
2026-02-04 14:24:49 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'https://black-end-xeak.onrender.com/';
 
 const api = axios.create({
     baseURL: BASE_URL,
